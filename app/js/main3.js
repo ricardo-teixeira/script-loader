31f7cf5e45244503f0735cfa74e2bd49fb7cec98
Ricardo Teixeira <ricardo_teixeira@hotmail.com.br>
2016-12-08 06:34:56 -0200

    stateChange function

--- app/js/main3.js
+++ app/js/main3.js
@@ -7,23 +7,24 @@ function loadScripts(arr, callback){
     var script = document.createElement("script")
     script.type = "text/javascript";
 
+    function stateChange() {
+      pendingCount--;
+      if (callback) {
+        callback(pendingCount);
+      }
+    }
+
     if (script.readyState){  //IE
       script.onreadystatechange = function(){
         if (script.readyState == "loaded" || script.readyState == "complete"){
           script.onreadystatechange = null;
-          pendingCount--;
-          if (callback) {
-            callback(pendingCount);
-          }
+          stateChange();
         }
       };
       document.write('<script src="' + url + '" defer></'+'script>');
     } else {  //Others
       script.onload = function(){
-        pendingCount--;
-        if (callback) {
-          callback(pendingCount);
-        }
+        stateChange();
       };
       script.src = url;
       document.getElementsByTagName("head")[0].appendChild(script);
